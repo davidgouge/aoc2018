15137962ef85d0e77509e78b6337911342b48910
Dave Gouge <davegouge@gmail.com>
2019-11-26 09:34:39 +0000

    Initial commit

---
+++ Day2/Program.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = @"efmyhuxcqqldtwjzvisepargvo
+efuyhuxckqldtwjrvrsbpargno
+efmyhuxckqlxtwjxvisbpargoo
+efmyhuxczqbdtwjzvisbpargjo
+efmyhugckqldtwjzvisfpargnq
+tfmyhuxckqljtwjzvisbpargko
+efmyhuxckqldtvuzvisbpavgno
+efmyhufcrqldtwjzvishpargno
+tfmyhuxbkqlduwjzvisbpargno
+efayhtxckqldbwjzvisbpargno
+efgyhuxckuldtwjzvisbpardno
+efmyhuxckuldtwizvisbpargqo
+efmyhuxcknldtjjzvihbpargno
+efmyhuxcnqddtwjzvisbpafgno
+efmyhubokqldtwjzvisbpargdo
+efmhhuxckqldtwdzvisbpjrgno
+efmyhuxckqldtwjrcisbpargny
+efmyhuxckqsdtwjzlisbpargng
+effyhuxckqlqtwjzjisbpargno
+nfmyhjxckqldtwjzcisbpargno
+efmyhvxckqldtwszvwsbpargno
+efmyhuxckqldtwutvisbpprgno
+kfmyhuxckqldtwzuvisbpargno
+efhyhtxckqldtwjmvisbpargno
+efmyhuhckqldtwjzvisbpxwgno
+efmyhuxcfqldtrjzvitbpargno
+efmyhudckqldtwjfvisbparvno
+ekmyhuxckqlstwjzvisbdargno
+efmyhuxckqlxtwjftisbpargno
+etsyhuxckqldtwjzvisbpargnf
+exmyhusckqldtwjzvisbpakgno
+efmyhubckqlrtljzvisbpargno
+efmyhuxckwldtwjovizbpargno
+efmyhulckqzdtwjzvisbpargpo
+efmyhuxckbcdlwjzvisbpargno
+zfmyhulckqbdtwjzvisbpargno
+efmyquxckfldtwazvisbpargno
+efxyhuxakqldtwjzvisupargno
+efmlhuxckkedtwjzvisbpargno
+efhyhuxwkqldtwjzvisbparjno
+efmyhuxfkqldtwjzvisvparyno
+efmyhuxckqfdtijzvisblargno
+efmyhuxckqldtfjzvisbwhrgno
+efmymuxcknldtwzzvisbpargno
+eomybuxckqldtwkzvisbpargno
+pfmyhuxckqldtwgzvasbpargno
+vfmyhuxcoqldtwjzvisbparvno
+eflyhuxckqldtwjzvirypargno
+efmyvuxckqldtwizvisbpaqgno
+epmyhuxckqldtwjzvesbparpno
+efoyhuxckoldtwjmvisbpargno
+efmyhuxckqydtwpzvisbpaqgno
+efmyhuxckqldezbzvisbpargno
+efmyhuxckqldtwjzvisboalxno
+efmyhuxckqldtwuzvipbjargno
+efmymuxcuqldtwjzvasbpargno
+jfmyhuxckqldtwjzvzsbpargdo
+nfmyhuxckqlntsjzvisbpargno
+efmxhuxckqgdtwjzvisbparjno
+efmyhuxckpldtpjzvpsbpargno
+efmyhuxcyqldtwjhvisbpargqo
+efmyhexgkqydtwjzvisbpargno
+ffmyhuxckqldtwjzvisbpafgnk
+efmyfuxckqldtwjpvisbpartno
+efmyhoxckcmdtwjzvisbpargno
+efmyhuxxkqldtwjzviabparyno
+jfmyhuxakqldtwgzvisbpargno
+efmjhuxckqcdtwjzvisbjargno
+efmyhuxccqldtwjzxisbxargno
+efmyhurckqldiwjzvrsbpargno
+efmyhuxckasdtwjzvisboargno
+efmyhvxckmldtwjgvisbpargno
+efmyhuxckoldtwjuvisbpardno
+efmyduxckqldtwjgvzsbpargno
+ejmyhuxckqldtwbzvisbpargnb
+efmymuxchqldtwjzvibbpargno
+efmyhjxckqldtwjgvinbpargno
+efmyhuxhyqldtwbzvisbpargno
+efmyhuxckqldtwjzvisbpzignq
+efmyuueckqldxwjzvisbpargno
+qfmyhyxckqldtwizvisbpargno
+efmyhupckqldtwjzvpgbpargno
+efmycuxckqldtwjzvfdbpargno
+efmyhugcrqldtwjfvisbpargno
+efmyhexckqldtwjzvischargno
+efmyhuxckqldtljzvasbpamgno
+efmyzdxckqldtwjovisbpargno
+efmyhuxccqldtwjzvdsbpaigno
+ufmyhuxekqldtwjzvisbpargne
+efmyhuxckqldfwozvisgpargno
+afmyhuackqldtwjzvisbdargno
+efmyauxckqldtwjzvisiparmno
+efmysuxckqldtwjzvisbeaggno
+efmyhuxckqldtwjzvisbgzigno
+efryhuxlkqldtwozvisbpargno
+lfmyhuxckqldtwjzvhsbparuno
+efmyhzxckqldswjzvisqpargno
+efmyhuxrkqldtwjzvisgpargco
+efmyhudckqldtwjzyisbkargno
+efmyhuacqqldtwjzviabpargno
+jfmyhuxckqldtwvzvicbpargno
+efmkhuxckqlftejzvisbpargno
+nfmyhuxckqldnwjzvisbxargno
+efmyhuxckqldtwjvvisjpyrgno
+efmyhuxcmxldtwjzvisbpargto
+efmyhuxckqldtwqbvpsbpargno
+efmyhuxckzldjwjzvisbplrgno
+efmywgxckqldtwxzvisbpargno
+efmsguxckqldhwjzvisbpargno
+nfmyhuxlkqldtwjzvisbgargno
+etmyhuxckqldtwjzvqsbptrgno
+efmyxuxckqldtfjzvisbyargno
+cfmihuxckqldtwjzvisbpargnf
+jfzyhuxckqldtwjzviscpargno
+efmyhuxckqldtmjzvisbpbzgno
+bfmyhuzckqldcwjzvisbpargno
+efmyhuxckqldtmjzvmslpargno
+efqyvuxckqldtwazvisbpargno
+efmecrxckqldtwjzvisbpargno
+efmyhuuckqldtwjzvisrpargnt
+efmphuxckqldtwjzvisbparmho
+ifmyhuxckqldtwjzvismpsrgno
+efmyhuookqldywjzvisbpargno
+efmyhfxckyldtwjnvisbpargno
+efmyhxhckqldtwjzvisqpargno
+efryhuxcfqldtwjzvisbparkno
+efmyhutckqldpwjzvixbpargno
+efmyoukckqldtwjzvisbpargko
+efmyhuxckqldtwjzviseparynv
+efmyhuxcksldvwjzvisbnargno
+efmyhuxckqrdtwlzmisbpargno
+efmyhuxcwqldtwjzviqapargno
+eymyhuxckqrdtwkzvisbpargno
+efmyhuxckqldtwjzpisopargnj
+efmyhuxikqldtwjzvirupargno
+efmyhuxcuzldtnjzvisbpargno
+efmyhxxikqldtwjzvisbpalgno
+efmyhuxceqldtwjzvdsbparguo
+efmyhuxwkqldtwjmvisbparxno
+efmyhuxpkqldtwjzvisfpargfo
+efmyfuxckaldtwjzvirbpargno
+efmyhuxckqrdtwjzvismprrgno
+efmyhuxckqldzwjzvisbpnrgfo
+efmyhfuckqldtwjyvisipargno
+efmyhuxcpqlqfwjzvisbpargno
+efmyyuxckqldtwjzvrsepargno
+efmphuxckqlptqjzvisbpargno
+efmyhuxnfqldtwjzvisbpmrgno
+efmyhuxckqldtwjzkisnpnrgno
+mfmyhuxckqldtwjzvisbzarcno
+efmyhuxckqldtwlzviszpargwo
+efmytuxckqndtwjqvisbpargno
+efmyzuxckqldtwjzvisbaargjo
+efmihuxckqlutwjzvimbpargno
+efmyhuxckqldgwjzvixbparono
+tfmyduxckqldtyjzvisbpargno
+ejmyhuockqldtwjzvidbpargno
+efmyheyckqkdtwjzvisbpargno
+efmyhuxckqldtwjzoisbpargfj
+efqyhuxcxqldtwxzvisbpargno
+jfmyhaxckqldtwjzvisbvargno
+hfmyhqxckqldtwjzvisbparvno
+efmyhukckqlrtwjzvqsbpargno
+efmyhuxckqldvwmzvisbparrno
+efoyhuxckqldtwjzvilwpargno
+ejmyhuxckqldtwjzxisbprrgno
+efmyhuxckqldtsjzvisupdrgno
+efzyhjxckqldtwjzvisbpasgno
+ebmyhulckqldtwjzvisbpargnr
+efmyhuxcjqlntwjzqisbpargno
+efmlocxckqldtwjzvisbpargno
+efmyhuxckqldtwjzvizkpargnm
+ebmyhuxckqldtwjzvlfbpargno
+efmyhuxckqldtwjyvisbpjrgnq
+afmyhuxckqldtwjzvpsbpargnv
+efmyxuxckqwdzwjzvisbpargno
+efmyhuxskqlqthjzvisbpargno
+efmyhuxckqldtwdzvisbearglo
+mfmyhuxckqldtzjzvisbparggo
+efmyhuqckqodtwjzvisbpadgno
+efmyhuxctqldywjzvisspargno
+efmyhuxckqqdtwjnvisbporgno
+efmyhixckqldowjzvisbpaagno
+efmyhuxckqldtsszvisbpargns
+edmyhuxckqpdtwjzrisbpargno
+efsyhuxckqldtijzvisbparano
+efmyhuxckqxdzwjzviqbpargno
+efmyhuxckqldtwjzviqqpsrgno
+efmyhuockqlatwjzvisbpargho
+efmyhuxckqldtwjzvishkavgno
+vfmyhuxckqldtwjzvksbaargno
+efmahuxckqudtwbzvisbpargno
+ewmyhixckqudtwjzvisbpargno
+efmywuxczqldtwjzvisbpargao
+efmyhuqjkqldtwyzvisbpargno
+efmyhuxekqldtwjzmksbpargno
+efmyhuxcoqtdtwjzvinbpargno
+ebmyhuxkkqldtwjzvisbdargno
+ecmyhnxckqldtwnzvisbpargno
+efmyhuxbkqldtwjzvksbpaigno
+efayhuxckqidtwjzvisbpavgno
+efmrhuxckqldswjzvisbpaugno
+efmyhuuckqldtwjyvisipargno
+xfmyhuxckqldawjzvosbpargno
+efmyhuxckklhtwjzvisbpargnq
+efmyhmxcaqldzwjzvisbpargno
+efiyhuxcksldtwjzvisbpamgno
+zfmyhuzckqldtwjzvisbparhno
+efmyhuxckqlvtwjdvisbparsno
+efmyhmxckaldtwjzmisbpargno
+efmysuxcqoldtwjzvisbpargno
+efmyhuxckqldtwjzvisbsargrb
+effyhuxckqldtwjzvisbpwfgno
+efmyhuxclqmdtwjzxisbpargno
+edmohuxckqldtwjziisbpargno
+efmyhuxckpldtwjzviubpaegno
+efmyhuxcpqldtwjzjimbpargno
+ehmyhuxckqldtwjzsisbpargnq
+efmyhcxcdqldtwjzvisbqargno
+efmjhuxckqldmwjzviybpargno
+efeyhzxckqlxtwjzvisbpargno
+efmyhuxczqadtwazvisbpargno
+efmahuxckqldtwjzvisbpafgnl
+efmyouxckqldtwjzvizbpacgno
+emmrhuxckqldtwjzvisqpargno
+exmyhuxckqlftwjnvisbpargno
+efuyhuxckqldrwjzvisbpargnw
+efmywuxfkqldtwjztisbpargno
+efmyhuxdkqldtwjzvisbpqrzno
+eemyhuxckqldrwjzvisbpajgno
+efmyiuxckqldtbjzvrsbpargno
+eqmyhuxckqldlwjzfisbpargno
+efmyhuxckqlitwuzvisbpvrgno
+ecoyhuxckqldtwjzvishpargno
+efmyhuxckcldtwjzlisbparlno
+efmyhsxcknldtwjfvisbpargno
+efmyhuxckqldtwjrvosbpargbo
+enmehuxckzldtwjzvisbpargno
+hfmyhuxckqqdtwjzvisbpawgno
+efmyhufckcjdtwjzvisbpargno
+efmxhuxckqldthjzvisfpargno
+efmyaukckqldtwjsvisbpargno
+efmyhukckqldtwpzvisbpmrgno
+dfmyhuxckqldtwjzvisbvarmno
+afmbhuxckqldtwjzvssbpargno
+efmyhuxchqldtwezvisbpargzo
+efmphuxckqlxjwjzvisbpargno
+efhyxuxckqldtwjzvisbpargko
+sfmyhexckqldtwjzvisbqargno
+efmghuxckqldtwjzvitbparnno";
+            PartOne(input);
+            PartTwo(input);
+        }
+
+        private static void PartTwo(string input)
+        {
+            string[] barcodes = input.Split(Environment.NewLine);
+            for (int x = 0; x < barcodes.Length; x++)
+            {
+                for (int y = 0; y < barcodes.Length; y++)
+                {
+                    if (AreBarcodesDifferentByOneCharacter(barcodes[x], barcodes[y]))
+                    {
+                        System.Console.WriteLine($"{barcodes[x]} and {barcodes[y]}");
+                    }
+                }
+            }
+        }
+
+        static bool AreBarcodesDifferentByOneCharacter(string barcode1, string barcode2)
+        {
+            var length = barcode1.Length;
+            var differentCharacters = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (barcode1[i] != barcode2[i])
+                {
+                    differentCharacters ++;
+                }
+            }
+
+            return differentCharacters == 1;
+        }
+
+        private static void PartOne(string input)
+        {
+            var threeLetterTotal = 0;
+            var twoLetterTotal = 0;
+            foreach (var barcode in input.Split(Environment.NewLine))
+            {
+                var scanResult = BoxScanner(barcode);
+                threeLetterTotal += scanResult.HasThreeLetters ? 1 : 0;
+                twoLetterTotal += scanResult.HasTwoLetters ? 1 : 0;
+            }
+
+            System.Console.WriteLine(threeLetterTotal * twoLetterTotal);
+        }
+
+        static ScanResult BoxScanner(string code)
+        {
+            var codeGroup = code.ToCharArray().GroupBy(c=> c);
+            var hasThreeLetters = codeGroup.Any(g => g.Count() == 3);
+            var hasTwoLetters = codeGroup.Any(g => g.Count() == 2);
+            return new ScanResult{HasThreeLetters = hasThreeLetters, HasTwoLetters = hasTwoLetters};
+        }
+    }
+    
+    class ScanResult
+    {
+        public bool HasThreeLetters { get; set; }
+        public bool HasTwoLetters { get; set; }
+    } 
+
+}
